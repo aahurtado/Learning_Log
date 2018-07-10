38125cdfb70fc529e56ec876213e0ceca77b1392
aahurtado <aaronhurtado95@gmail.com>
2018-07-10 03:06:52 -0700

    first commit

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+"""Defines URL patterns for users"""
+from django.conf.urls import url
+from django.contrib.auth.views import login
+from django.urls import path
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    # Login page
+    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+]
